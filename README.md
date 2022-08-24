26751bc8a21c072d574683b24825f38720133074
LittleLittleKang <tan452301@live.com>
2022-08-24 15:45:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KKSpmLogger
+
+A description of this package.
---
+++ Sources/KKSpmLogger/KKSpmLogger.swift
@@ -0,0 +1,10 @@
+public struct KKSpmLogger {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func sayHello() {
+        print("KKSpm say hello")
+    }
+}
