1591497c7dfac51dbdba64b5694c9618d34614b2
LittleLittleKang <tan452301@live.com>
2022-08-24 17:24:58 +0800

    添加AFN

--- Sources/KKSpmLogger/KKSpmLogger.swift
+++ Sources/KKSpmLogger/KKSpmLogger.swift
@@ -1,3 +1,5 @@
+import AFNetworking
+
 public struct KKSpmLogger {
     public private(set) var text = "Hello, World!"
 
@@ -6,5 +8,20 @@ public struct KKSpmLogger {
     
     public func sayHello() {
         print("KKSpm say hello")
+    }
+    
+    private func httpRequest() {
+        
+        let configuration = URLSessionConfiguration.default
+        let manager = AFURLSessionManager.init(sessionConfiguration: configuration)
+
+        let url = URL.init(string: "http://example.com/download.zip")
+        let request = URLRequest.init(url: url!)
+
+        let downloadTask = manager.downloadTask(with: request, progress: nil, destination: nil, completionHandler: { response, url, error in
+            print("下载结果: \(response)")
+        })
+
+        downloadTask.resume()
     }
 }
